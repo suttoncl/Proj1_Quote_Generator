c18544063431f670e6d6b24985ff097f458827a8
suttoncl <suttoncl04@yahoo.com>
2020-05-14 08:37:40 -0400

    Final Version
    Updated comments throughout the script. Also, cleaned up script by making the line spacing between blocks of code uniform throughout the script.

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * A list of inpsiring and motivating quotes, which are placed in a `quotes` array 
 ***/
 var quotes = [
   {
@@ -43,31 +43,40 @@ var quotes = [
   }
 ];
 
+/*** 
+ * A test to make sure the quotes properly print to the console after being defined
+ * Statement can be uncommented whenever it is needed for use
+***/
 //console.log(quotes);
 
+/*** 
+ * Variables to be used in a function that will be defined later
+***/
 var displayed_quote; 
 var object_elements_message;
 
 /***
- * `getRandomQuote` function
+ * A function that will randomly generate a number between 0 and 6
+ * The number will then be used to return the object at that index location
+ * Also includes a statement to automatically log the number to the console. Can help with testing to ensure the function is printing numbers within the correct range 
 ***/
 function getRandomQuote() {
   var randomNumber = Math.floor(Math.random() * quotes.length);
   console.log(randomNumber);
   return quotes[randomNumber];
-  
 }
 
+/***
+ * A test to make sure that, when called, the `getRandomQuote` function properly returns the object stored in a particular location within the array 
+ * Statement can be uncommented whenever it is needed for use
+ ***/ 
 //console.log(getRandomQuote());
 
-// function print(object_elements_message){
-//   var outputDiv = document.getElementById('quote-box');
-//   outputDiv.innerHTML = object_elements_message;
-// }
-
-
 /***
- * `printQuote` function
+ * Takes the object returned from the `getRandomQuote` function and stores its contents in a variable
+ * A separate variable is used to build the HTML message by specfically calling each of the object's properties
+ * Conditional statements are included to catch properties that might not be defined in all of the array's objects
+ * The final message is then printed to the page by leveraging a function and a property performed on the document object
 ***/
 function printQuote(){
   displayed_quote = getRandomQuote();
@@ -81,13 +90,14 @@ function printQuote(){
     object_elements_message += '<span class="year">' + displayed_quote.year + '</span>';
     }
   object_elements_message += '</p>';
-  //return object_elements_message;
   document.getElementById('quote-box').innerHTML = object_elements_message;  
 }
 
+/*** 
+ * A test to make sure that the `getRandomQuote` function properly works with the HTML message being constructed, as well as the conditional statements
+ * Statement can be uncommented whenever it is needed for use
+***/
 //console.log(printQuote());
-//printQuote();
-
 
 /***
  * click event listener for the print quote button
